ef7aced1dd5e3ca66c7cf72b3a5b08f5841bf747
tiana2829 <ram.mamitiana@gmail.com>
2024-11-18 22:04:56 +0100

    Update

--- javascript/script.js
+++ javascript/script.js
@@ -4,14 +4,20 @@ const LOGIN_LINK = document.querySelector('#login-link');
 const MODIFY_BUTTON = document.querySelector('.modifyButton');
 const MAIN_MODAL = document.querySelector('#mainModal');
 const SECOND_MODAL = document.querySelector('#secondModal');
-const worksWithIconTrash = document.querySelector('.worksWithIconTrash');
+const WORKS_WITH_ICON_TRASH = document.querySelector('.worksWithIconTrash');
 const ADDPICTURE = document.querySelector('.addPicture');
-
+const FORM_MODAL = document.querySelector('#formModal');
+const fileInput = document.querySelector('#fileUpload');
+const imgPreview = document.querySelector('#imagePreview');
+const addPhotoButton = document.querySelector('.addPhoto');
+const iconPreview = document.querySelector('#iconPreview');
+const extensions = document.querySelector('.extensions');
 const API_WORKS = 'http://localhost:5678/api/works';
 const API_CATEGORIES = 'http://localhost:5678/api/categories';
 
 // Tableau pour stocker les data
 let allWorks = [];
+let allCategories = [];
 
 /**
  * Crée une balise figure contenant une image et une caption pour un travail donné.
@@ -50,6 +56,7 @@ const fetchWorksAndCategories = async () => {
     allWorks = await worksResponse.json();
     const categories = await categoriesResponse.json();
 
+    allCategories = categories;
     categories.unshift({ id: 0, name: 'Tous' });
     displayWorks(allWorks);
     populateFilters(categories);
@@ -171,34 +178,65 @@ LOGIN_LINK.addEventListener('click', handleLoginClick);
 fetchWorksAndCategories();
 checkUserStatus();
 
+// //Fonction pour récupérer les images de l'API et y mettre le logo poubelle
+// let trash = [];
+// let snaps = [];
+// const populateMainModal = async () => {
+//   try {
+//     const works = await fetch(API_WORKS); // récupération des données depuis l'API
+//     let worksData = await works.json();
+//     WORKS_WITH_ICON_TRASH.innerHTML = '';
+//     worksData.forEach((workData) => {
+//       let img = document.createElement('img');
+//       img.src = workData.imageUrl;
+//       snaps[workData.id] = document.createElement('figure');
+//       snaps[workData.id].appendChild(img);
+//       trash[workData.id] = document.createElement('i');
+//       trash[workData.id].classList.add('fa-solid', 'fa-trash-can', 'trash');
+//       snaps[workData.id].appendChild(trash[workData.id]);
+//       WORKS_WITH_ICON_TRASH.appendChild(snaps[workData.id]);
+//       let url = `http://localhost:5678/api/works/${workData.id}`;
+
+//       trash[workData.id].addEventListener('click', () => {
+//         deleteWork(url);
+//         MAIN_MODAL.style.display = 'none';
+//       });
+//     });
+//   } catch (error) {
+//     console.error('error fetching work', error);
+//     throw new Error('Error fetching work data', error);
+//   }
+// };
+
 //Fonction pour récupérer les images de l'API et y mettre le logo poubelle
 let trash = [];
 let snaps = [];
 const populateMainModal = async () => {
   try {
-    const works = await fetch(API_WORKS); // récupération des données depuis l'API
-    let worksData = await works.json();
-    worksWithIconTrash.innerHTML = '';
-    worksData.forEach((workData) => {
-      let img = document.createElement('img');
-      img.src = workData.imageUrl;
-      snaps[workData.id] = document.createElement('figure');
-      snaps[workData.id].appendChild(img);
-      trash[workData.id] = document.createElement('i');
-      trash[workData.id].classList.add('fa-solid', 'fa-trash-can', 'trash');
-      snaps[workData.id].appendChild(trash[workData.id]);
-      worksWithIconTrash.appendChild(snaps[workData.id]);
-      let url = `http://localhost:5678/api/works/${workData.id}`;
+    const worksData = await fetch(API_WORKS).then((res) => res.json());
+    WORKS_WITH_ICON_TRASH.innerHTML = '';
 
-      trash[workData.id].addEventListener('click', () => {
-        deleteWork(url);
-        MAIN_MODAL.style.display = 'none';
-      });
+    worksData.forEach((workData) => {
+      const figure = createWorkFigureWithTrash(workData);
+      WORKS_WITH_ICON_TRASH.appendChild(figure);
     });
   } catch (error) {
-    console.error('error fetching work', error);
-    throw new Error('Error fetching work data', error);
+    console.error('Error fetching works', error);
   }
+};
+
+const createWorkFigureWithTrash = (workData) => {
+  const figure = createWorkFigure(workData);
+  const trashIcon = document.createElement('i');
+  trashIcon.classList.add('fa-solid', 'fa-trash-can', 'trash');
+  figure.appendChild(trashIcon);
+
+  trashIcon.addEventListener('click', () => {
+    deleteWork(`http://localhost:5678/api/works/${workData.id}`);
+    MAIN_MODAL.style.display = 'none';
+  });
+
+  return figure;
 };
 
 const deleteWork = async (url) => {
@@ -237,8 +275,120 @@ MODIFY_BUTTON.addEventListener('click', async () => {
   await populateMainModal();
 });
 //On ouvre la seconde modale
-ADDPICTURE.addEventListener('click', async () => {
+// ADDPICTURE.addEventListener('click', async () => {
+//   SECOND_MODAL.style.display = 'flex';
+//   //on cache le premier modale
+//   MAIN_MODAL.style.display = 'none';
+// });
+
+//On ouvre la seconde modale
+/*ADDPICTURE.addEventListener('click', async () => {
   SECOND_MODAL.style.display = 'flex';
-  //on cache le premier modale
   MAIN_MODAL.style.display = 'none';
+
+  try {
+    const [worksData, categoriesData] = await Promise.all([
+      fetchWorks(),
+      fetchCategories(),
+    ]);
+    populateWorks(worksData);
+    populateCategories(categoriesData);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données', error);
+  }
 });
+
+const fetchWorks = async () => {
+  const response = await fetch(API_WORKS);
+  return response.json();
+};
+
+const fetchCategories = async () => {
+  const response = await fetch(API_CATEGORIES);
+  return response.json();
+};
+
+const populateWorks = (worksData) => {
+  WORKS_WITH_ICON_TRASH.innerHTML = '';
+  worksData.forEach((workData) =>
+    WORKS_WITH_ICON_TRASH.appendChild(createWorkFigureWithTrash(workData))
+  );
+};
+
+const populateCategories = (categoriesData) => {
+  const categorySelect = document.getElementById('category');
+  categorySelect.innerHTML = '';
+  categoriesData.forEach((category) => {
+    const option = document.createElement('option');
+    option.value = category.id;
+    option.text = category.name;
+    categorySelect.appendChild(option);
+  });
+};
+
+FORM_MODAL.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const imageInput = document.getElementById('fileUpload');
+  const titleInput = document.getElementById('title');
+  const categorySelect = document.getElementById('category');
+
+  const formData = new FormData();
+  formData.append('image', imageInput.files[0]);
+  formData.append('title', titleInput.value);
+  formData.append('categoryId', categorySelect.value);
+
+  try {
+    const token = localStorage.getItem('token');
+    const response = await fetch(API_WORKS, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    // Log the response to check what is returned
+    const newWork = await response.json();
+    console.log('New work added:', newWork);
+
+    // add picture to gallery
+    await fetchWorksAndCategories();
+    alert('picture added');
+    //SECOND_MODAL.style.display = "none"; // Close the modal
+  } catch (error) {
+    console.error('Error adding picture:', error);
+  }
+});*/
+
+// Événement pour l'ouverture de la modal sans appel API
+ADDPICTURE.addEventListener('click', () => {
+  openSecondModal();
+  selectCategories();
+});
+// Fonction pour gérer l'ouverture de la modal
+const openSecondModal = () => {
+  SECOND_MODAL.style.display = 'flex';
+  MAIN_MODAL.style.display = 'none';
+};
+
+// Fonction pour afficher les catégories dans le select
+const selectCategories = () => {
+  let categorySelect = document.getElementById('category');
+  categorySelect.innerHTML = '';
+  const option = document.createElement('option');
+  option.value = '';
+  option.innerHTML = '';
+  categorySelect.appendChild(option);
+  console.log('toutes les catégories sont',allCategories)
+const allCategoriesWithoutTous = allCategories.filter((cat)=>cat.id != 0)
+  allCategoriesWithoutTous.forEach((category) => {
+    const option = document.createElement('option');
+    option.id = category.id;
+    option.value = category.name;
+    option.innerHTML = category.name;
+    categorySelect.appendChild(option);
+  });
+};
